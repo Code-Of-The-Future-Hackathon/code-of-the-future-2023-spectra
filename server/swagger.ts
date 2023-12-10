8ef09d6e5392e40e52177cee73a95a77b3496894
Deivid Donchev <85338879+DNDonchev20@users.noreply.github.com>
2023-12-10 09:07:13 +0200

    Fix the azure link

--- server/swagger.ts
+++ server/swagger.ts
@@ -11,7 +11,7 @@ const options = {
         servers: [
             {
                 url: 'http://localhost:3000',
-                https: '//spectra-webapi.azurewebsites.net/api/'
+                https: '//spectra-webapi.azurewebsites.net'
             },
         ],
     },
